4e960ef1037e1bdee7a3e052fe88e96b4da92873
igorcb <igor.batista@gmail.com>
2013-06-04 18:14:45 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fujiame::Application.config.secret_token = 'fd127b27544eca09534a91b37bb28ccdc8952f43450059ab87b558d7fb40380179a92e9e2319005b5cf04ab61eb04b8c01850a2372d08c3dfc4f0ed317c2db91'
